11355c69baa3bb016982306872f6ca0f1cf8f8e2
ShreyasTD <shreyas72045@gmail.com>
2026-08-11 19:24:16 +0530

    Completed pointer problem

---
+++ 7.1 Day 12 prob continue.py
@@ -0,0 +1,37 @@
+#Count the number of pairs in  a sorted list which sums to target.
+"""a=[1,2,3,4,5,6,7,8,9,10]
+left =0
+right=len(a)-1
+target =15
+count=0
+
+while left <right:
+    if a[left] +a[right]== target:
+        count+=1
+        left+=1
+        right+=1
+     
+    elif[left] +a[right]> target:
+     right-=1
+    
+    else:
+        left+=1
+
+print(count)
+"""
+#remove duplicates from an sorted arrray/list
+a=[1,1,2,2,3]
+res=[]
+for i in a:
+  if i not in res:
+     res.append(i)
+print(res)
+
+#slove and fast pointer tech
+a=[1,2,2,2,3,3,4,5,5,6,6]
+slow=0
+for fast in range(1,len(a)):
+   if a[fast]!=a[slow]:
+      slow+=1
+      a[slow]=a[fast]
+print(a[:slow+1])
