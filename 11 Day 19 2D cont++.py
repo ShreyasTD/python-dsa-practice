c86b5ee198935791cb5cb2cd46d08abca12d357d
ShreyasTD <shreyas72045@gmail.com>
2026-08-16 21:17:40 +0530

    2D list complit

--- 11 Day 19 2D cont++.py
+++ 11 Day 19 2D cont++.py
@@ -17,7 +17,7 @@ def primaryDiagnalSum(a):
     return total
 print(primaryDiagnalSum([[1,2,3],[4,5,6,],[7,8,9]]))
 
-#print all the boundrey element in 2d list
+#2.1 print all the boundrey element in 2d list
 def primaryDiagnalSum(a):
     total=0
     for i in range(len(a)):
@@ -25,3 +25,16 @@ def primaryDiagnalSum(a):
           if i==0 or j==0 or i==len(a) or j==len(a[0]):
              return total
 print(primaryDiagnalSum([[1,2,3],[4,5,6,],[7,8,9]]))
+
+#3 Rotate a matrix by 90 deg clockwise
+def rotateClockwise(a):
+    for i in range(len(a)):
+        for j in range(i+1,len(a[0])):
+            a[i][j],a[j][i]=a[j][i],a[i][j]
+    
+    for i in range(len(a)):
+        for j in range(len(a[0])-1,-1,-1):
+            print(a[i][j],end=" ")
+        print()
+
+rotateClockwise([[1,2,3],[4,5,6],[7,8,9]])
