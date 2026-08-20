2a36bdc179c5bf48759bbe0de6e0d8a3d4677cbd
ShreyasTD <shreyas72045@gmail.com>
2026-08-20 17:44:54 +0530

    binary serch to target number

---
+++ 11. Day 21 binary serch.py
@@ -0,0 +1,81 @@
+a=[10,20,30,40,50,60]
+found=False
+for i in range (len(a)):
+    if a[i]==found:
+        found=True
+        break;
+print(a)
+"""
+#banarey serch
+a=[1,2,3,4,5,6,7,12,18,30]
+left=0
+right=len(a)-1
+found=False
+while left<=right:
+    mid=(left+right)/2
+    if a[mid]==target:
+        found=True
+        break
+    elif a[mid]>target:
+            right=mid-1
+    else:
+         left=mid+1
+"""
+#create a function which takes a sorted list and a target and returns a Boolean (T/F) if the target exist in the list.
+def bs(a,target):
+    left=0
+    right = len(a)-1
+    found = False
+    while left<=right:
+        mid=(left+right)//2
+        if a[mid]==target:
+            found=True
+            return found
+        elif a[mid]>target:
+            right=mid-1
+        else:
+            left=mid+1
+    return found
+
+print(bs([1,2,3,4,5],2))
+
+#Find the first occurence of target in a sort list.
+#[1,2,2,5,6,77,77,100], target = 77
+#output :  5
+
+def firstOccurence(a,target):
+    left=0
+    right = len(a)-1
+    ans=-1
+    while left<=right:
+        mid=(left+right)//2
+        if a[mid]==target:
+            ans=mid
+            right=mid-1
+        elif a[mid]>target:
+            right=mid-1
+        else:
+            left=mid+1
+    return ans
+
+print(firstOccurence([1,2,3,3,3,3,3,4],2))
+
+#return the last occurence of target in a sorted list
+def lastOccurence(a,target):
+    left=0
+    right = len(a)-1
+    ans=-1
+    while left<=right:
+        mid=(left+right)//2
+        if a[mid]==target:
+            ans=mid
+            left=mid+1
+        elif a[mid]>target:
+            right=mid-1
+        else:
+            left=mid+1
+    return ans
+
+
+print(lastOccurence([1,2,3,3,3,3,3,4],3))
+
