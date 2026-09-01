8d1722e82a665598053f11c3df877509987c4890
ShreyasTD <shreyas72045@gmail.com>
2026-09-01 17:10:48 +0530

    example

--- nee.py
+++ nee.py
@@ -2,3 +2,20 @@ count = 0
 while count < 3:
     print("Looping...")
     count = count +1
+
+a="string"
+print(a)
+a=23
+print(a)
+
+winter_cloth= "havey joket"
+print(winter_cloth)
+winter_cloth= "scarf"
+print(winter_cloth)
+import keyword
+print(keyword.kwlist)
+temp=15
+if temp>20:
+    print("it is warm")
+else:
+     print("It is chilly")
